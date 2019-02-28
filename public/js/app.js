bc70a5158cfc8fc7014c967a89c6facaabbb9418
zhangzhen <zhangzhen@magicmoma.com>
2019-02-28 16:01:41 +0800

    style:change default style

--- public/js/app.js
+++ public/js/app.js
@@ -49143,8 +49143,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/momaer/webproject/blog/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/momaer/webproject/blog/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /var/www/blog/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /var/www/blog/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
